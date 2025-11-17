96fcfc1d064a37fc695a6dfdd6ddd7a584877619
Ahtisham Bashir <ahtishamdoggar@gmail.com>
2025-11-18 00:00:32 +0500

    Added the get high light function

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,19 +34,59 @@ const updateStats = () => {
   roundElement.textContent = round;
 };
 
-const updateRadioOption = (index, scoreVal) => {
+const updateRadioOption = (index, score) => {
   scoreInputs[index].disabled = false;
-  scoreInputs[index].value = scoreVal;
-  scoreSpans[index].textContent = `, score = ${scoreVal}`;
-}
+  scoreInputs[index].value = score;
+  scoreSpans[index].textContent = `, score = ${score}`;
+};
+
+const getHighestDuplicates = (arr) => {
+  const counts = {};
+
+  for (const num of arr) {
+    if (counts[num]) {
+      counts[num]++;
+    } else {
+      counts[num] = 1;
+    }
+  }
+
+  let highestCount = 0;
+
+  for (const num of arr) {
+    const count = counts[num];
+    if (count >= 3 && count > highestCount) {
+      highestCount = count;
+    }
+    if (count >= 4 && count > highestCount) {
+      highestCount = count;
+    }
+  }
+
+  const sumOfAllDice = arr.reduce((a, b) => a + b, 0);
+
+  if (highestCount >= 4) {
+    updateRadioOption(1, sumOfAllDice);
+  }
+
+  if (highestCount >= 3) {
+    updateRadioOption(0, sumOfAllDice);
+  }
+
+  updateRadioOption(5, 0);
+};
+
+
 
 rollDiceBtn.addEventListener("click", () => {
   if (rolls === 3) {
     alert("You have made three rolls this round. Please select a score.");
   } else {
+
     rolls++;
     rollDice();
     updateStats();
+    getHighestDuplicates(diceValuesArr);
   }
 });
 
